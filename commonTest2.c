69fc4689ebf17e3baa09f72cfab4db2f8b52cf0b
John Sanford <jpsanford@comcast.net>
2018-08-16 10:12:33 -0400

    Initial code.

---
+++ commonTest2.c
@@ -0,0 +1,28 @@
+/* source module: commonTest3.c */
+
+void commonTest2Func (void)
+{
+    
+}
+
+void commonTest2Func2 (void)
+{
+    
+}
+
+void commonTest2Func3 (void)
+{
+    
+}
+
+
+void commonTest2Func4 (void)
+{
+    
+}
+
+
+void commonTest2Func5 (void)
+{
+    
+}
---
+++ commonTest2.h
@@ -0,0 +1,10 @@
+/* header file commonTest3.h */
+
+#define COMMON_TEST_2_VALUE_1        (1)
+
+/*** function prototypes **/
+void commonTest2Func (void);
+void commonTest2Func2 (void);
+void commonTest2Func3 (void);
+void commonTest2Func4 (void);
+void commonTest2Func5 (void);
